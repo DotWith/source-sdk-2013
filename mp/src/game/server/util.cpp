5f665ca661e515b0237df79dce22c00d22147f65
DotWithATopHat <52358442+DotWith@users.noreply.github.com>
2022-04-22 03:16:09 -0400

    NPCs Fixed

--- mp/src/game/server/util.cpp
+++ mp/src/game/server/util.cpp
@@ -637,26 +637,66 @@ CBasePlayer* UTIL_PlayerByUserId( int userID )
 }
 
 //
-// Return the local player.
-// If this is a multiplayer game, return NULL.
+// Return the listenserver-host.
+// If that's not possible, try to return literally ANY player
 // 
 CBasePlayer *UTIL_GetLocalPlayer( void )
 {
-	if ( gpGlobals->maxClients > 1 )
-	{
-		if ( developer.GetBool() )
-		{
-			Assert( !"UTIL_GetLocalPlayer" );
-			
-#ifdef	DEBUG
-			Warning( "UTIL_GetLocalPlayer() called in multiplayer game.\n" );
-#endif
-		}
-
-		return NULL;
+	//try to return the listenserver-host
+	CBasePlayer *pHost = UTIL_GetListenServerHost();
+	if (pHost){
+		return pHost;
 	}
 
-	return UTIL_PlayerByIndex( 1 );
+	//try to return literally any other client on the server
+	for (int i = 1; i < gpGlobals->maxClients; i++)
+	{
+		CBasePlayer *pPlayer = UTIL_PlayerByIndex(i);
+
+		if (pPlayer){
+			return pPlayer;
+		}	
+	}
+
+	return NULL;
+}
+
+//
+// Returns nearest player. 
+// Control with boolean if line of sight is needed.
+//
+CBasePlayer *UTIL_GetNearestPlayer(CBaseEntity *pLooker, bool bNeedsLOS)
+{
+	float flFinalDistance = 999999.0f;
+	CBasePlayer *pFinalPlayer = NULL;
+
+	for (int i = 1; i < gpGlobals->maxClients; i++)
+	{
+		CBasePlayer *pPlayer = UTIL_PlayerByIndex(i);
+
+		if (!pPlayer){
+			continue;
+		}
+
+		float flDistance = (pPlayer->GetAbsOrigin() - pLooker->GetAbsOrigin()).LengthSqr();
+
+		if (flDistance < flFinalDistance)
+		{
+			if (bNeedsLOS)
+			{
+				//Check if the player is visible to the entity (only brushes obstruct vision)
+				if (!pLooker->FVisible(pPlayer, MASK_SOLID_BRUSHONLY))
+				{
+					continue;
+				}				
+			}
+			
+			pFinalPlayer = pPlayer;
+			flFinalDistance = flDistance;
+		}
+	}
+
+	return pFinalPlayer;
 }
 
 //
